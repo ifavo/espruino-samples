33119cec10283e9e3fe16f9b37e8b326787481d0
ifavo <mario@favo.org>
2014-11-08 14:19:04 +0100

    servo moves on http requests

--- sensor-test.js
+++ sensor-test.js
@@ -10,6 +10,7 @@
  * - onboard LEDs
  * - LEDs
  * - speaker
+ * - servo
  *
  * otherwise it tests:
  * - wifi with cc3000
@@ -35,7 +36,8 @@ var config = {
 		},
 		distance: {
 			led: [C6, C7, C8]
-		}
+		},
+		servo: A15
 	},
 	
 	wlan: {
@@ -48,6 +50,10 @@ var config = {
 	}
 };
 
+
+// global vars for whatever is connected
+var motion, distance, servo, wlan;
+
 function init() {
 
 	// startup visualization with internal LEDs
@@ -59,7 +65,7 @@ function init() {
 	// connecting motion sensor
 	if ( config.sensor.motion ) {
 		console.log("connecting motion sensor...");
-		var motion = setWatch(function (e) {
+		motion = setWatch(function (e) {
 			config.status.motion = e;
 			console.log("motion detected", e);
 			analogWrite(config.output.motion.led,1.0);
@@ -79,7 +85,7 @@ function init() {
 	// connecting distance sensor
 	if ( config.sensor.distance ) {
 		console.log("connecting distance sensor...");
-		var distance = require("HC-SR04").connect(config.sensor.distance[0],config.sensor.distance[1],function(dist) {
+		distance = require("HC-SR04").connect(config.sensor.distance[0],config.sensor.distance[1],function(dist) {
 			config.status.distance = Number(dist);
 	
 			if ( config.status.distance < 10 ) {
@@ -105,10 +111,17 @@ function init() {
 		});
 	}
 
+	// connecting servo
+	if ( config.output.servo ) {
+		console.log("connecting servo...");
+		servo = require("servo").connect(config.output.servo);
+		servo.move(0);
+	}
+
 	// connecting wlan
 	if ( config.wlan ) {
 		console.log("connecting wlan...");
-		var wlan = require("CC3000").connect();
+		wlan = require("CC3000").connect();
 		wlan.connect( config.wlan.sid, config.wlan.password, function (s) { 
 			if ( s == "dhcp" ) {
 				config.status.wlan = wlan.getIP();
@@ -154,6 +167,12 @@ function intervalFunc() {
 function onPageRequest(req, res) { 
 	res.writeHead(200, {"Content-Type": "application/json"});
 	res.end(JSON.stringify(config.status));
+	
+	if ( servo ) {
+		servo.move(0, 1000, function() {
+		  servo.move(1, 1000);
+		});
+	}
 }
 
 
